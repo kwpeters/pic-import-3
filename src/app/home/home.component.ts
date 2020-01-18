e0ac9b4eed2438b6f9b5848fd7054859f79e373c
Kevin Peters <kwpeters@gmail.com>
2020-01-18 13:43:48 -0500

    Got app working using depot files (native promises).

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {Directory} from "../depot/directory"
+
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit
+{
+    public messagePromise: Promise<string>;
 
-  constructor() { }
+    constructor() {
 
-  ngOnInit(): void { }
+        const tmpDirPath = "/Users/kwpeters/tmp";
+        const tmpDir = new Directory(tmpDirPath);
+
+        this.messagePromise = tmpDir.exists()
+        .then((tmpDirStats) => {
+            return `tmp directory: ${JSON.stringify(tmpDirStats)}`;
+        });
+
+    }
+
+    ngOnInit(): void { }
 
 }
