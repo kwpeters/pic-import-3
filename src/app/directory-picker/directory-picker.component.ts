0986260effd3176b6e5695896a6e6a02ba954d68
Kevin Peters <kwpeters@gmail.com>
2020-01-22 07:31:39 -0500

    Added directory picker component.

---
+++ src/app/directory-picker/directory-picker.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directory-picker',
+  templateUrl: './directory-picker.component.html',
+  styleUrls: ['./directory-picker.component.scss']
+})
+export class DirectoryPickerComponent implements OnInit {
+
+  constructor()
+  {
+  }
+
+  ngOnInit()
+  {
+  }
+
+}
